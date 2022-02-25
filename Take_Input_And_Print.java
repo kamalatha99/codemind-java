41fa34c8b4b2938189d828a508d08f67235fb8de
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2022-02-25 19:36:13 +0530

    import java.util.*; class Stri {     public static void main(String args[])     {         Scanner obj=new Scanner(System.in);         String c=obj.nextLine();         System.out.println(c);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Stri
+{
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        String c=obj.nextLine();
+        System.out.println(c);
+    }
+}
