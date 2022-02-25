92380f3e3a3e50f2c3504f69bafa8cc857f4d959
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2022-02-25 19:32:37 +0530

    import java.util.*; class Modula {     public static void main(String args[])     {         Scanner obj=new Scanner(System.in);         int a=obj.nextInt();         int b=obj.nextInt();         int c=a%b;         System.out.println(c);     } }

---
+++ Modulus.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Modula
+{
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        int a=obj.nextInt();
+        int b=obj.nextInt();
+        int c=a%b;
+        System.out.println(c);
+    }
+}
