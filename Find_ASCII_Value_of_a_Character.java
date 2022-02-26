bfc3937e366521524c27b6f9a0959a7f9eebccb8
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2022-02-26 22:07:45 +0530

    import java.util.*; class Aschi {     public static void main(String args[])     {         Scanner obj=new Scanner(System.in);         char ch=obj.next().charAt(0);         int b=(int)ch;         System.out.println(b);
        }
    }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Aschi
+{
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        char ch=obj.next().charAt(0);
+        int b=(int)ch;
+        System.out.println(b);
+        
+    }
+}
