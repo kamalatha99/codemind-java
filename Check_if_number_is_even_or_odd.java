c7e270fa857132d0785670e443e2dda3677d9424
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2022-02-27 11:32:13 +0530

    import java.util.*; class Coin {
    	public static void main(String[] args) {
    		// TODO Auto-generated method stub
    		  Scanner obj=new Scanner(System.in);
    	        int n=obj.nextInt();
    	        if(n%2==0)
    	        {
    	            System.out.println("Even");
    	        }
    	        else
    	        {
    	            System.out.println("Odd");
    	        }
    	}
    }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Coin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner obj=new Scanner(System.in);
+	        int n=obj.nextInt();
+	        if(n%2==0)
+	        {
+	            System.out.println("Even");
+	        }
+	        else
+	        {
+	            System.out.println("Odd");
+	        }
+	}
+}
