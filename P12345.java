354965383fa0df2deb52b8768ccc0a5dbcdb388c
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2022-03-07 07:36:16 +0530

    import java.util.*;

---
+++ P12345.java
@@ -0,0 +1,24 @@
+package phase2;
+import java.util.*;
+public class P12345 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+				Scanner obj=new Scanner(System.in);
+		        int n=obj.nextInt();
+		        for(int i=n;i>=1;i--)
+		        {
+		            for(int j=1;j<=i;j++)
+		            {
+		                System.out.print(j);
+		                
+		            }
+		            System.out.println();
+		            
+		        }
+		    }
+		
+
+	}
+
+
