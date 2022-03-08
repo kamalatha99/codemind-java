2c6277e75f96c76062054919a27dedb281563809
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2022-03-08 12:58:45 +0530

    String

---
+++ DefaulInput.java
@@ -0,0 +1,14 @@
+package phase2;
+import java.util.*;
+public class DefaulInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner obj=new Scanner(System.in);
+        String s1="Hello Technicalhub";
+        String s=obj.nextLine();
+        System.out.println(s1);
+        System.out.println(s);
+	}
+
+}
