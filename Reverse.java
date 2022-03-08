c6e04a6feb85560ff9b7dc9856f095de58f0538d
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2022-03-08 21:22:45 +0530

    String

---
+++ Reverse.java
@@ -0,0 +1,13 @@
+package phase2;
+import java.util.*;
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner obj=new Scanner(System.in);
+	        StringBuffer sb=new StringBuffer();
+	        sb.append(obj.nextLine());
+	        System.out.println(sb.reverse());
+	}
+
+}
