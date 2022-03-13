75a9446863164ee5b168ce18dde37684c0a34731
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2022-03-13 09:32:47 +0530

    import java.util.*; class Npattern {     public static void main(String args[])     {         Scanner obj=new Scanner(System.in);         int n=obj.nextInt();         for(int i=0;i<n;i++)         {             for(int j=0;j<n;j++)             {                 if(i==j||j==0 ||j==n-1)                 {                     System.out.print("*"+" ");                 }                 else                 {                     System.out.print("  ");                 }             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,19 +1,24 @@
 import java.util.*;
-class Pattern
+class Npattern
 {
     public static void main(String args[])
     {
         Scanner obj=new Scanner(System.in);
         int n=obj.nextInt();
-        for(int i=n;i>=1;i--)
+        for(int i=0;i<n;i++)
         {
-            for(int j=1;j<=i;j++)
+            for(int j=0;j<n;j++)
             {
-                System.out.print(j);
-                
+                if(i==j||j==0 ||j==n-1)
+                {
+                    System.out.print("*"+" ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
             }
             System.out.println();
-            
         }
     }
 }
